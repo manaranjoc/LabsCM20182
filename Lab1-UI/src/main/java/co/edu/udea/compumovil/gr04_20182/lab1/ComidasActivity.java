11892866dec2783f0104a1a55e5b07915443e238
Miguel Angel <manaranjoc@alcazares.edu.co>
2018-08-22 17:10:04 -0500

    Agregar Galeria al Boton para escoger Imagen del Plato

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20182/lab1/ComidasActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20182/lab1/ComidasActivity.java
@@ -1,16 +1,66 @@
 package co.edu.udea.compumovil.gr04_20182.lab1;
 
+import android.Manifest;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.ArrayList;
+
+import gun0912.tedbottompicker.TedBottomPicker;
 
 public class ComidasActivity extends AppCompatActivity {
-
+    
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comidas);
+
+        Button button = findViewById(R.id.button_gallery);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PermissionListener permissionlistener = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+                        Toast.makeText(ComidasActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                        TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(ComidasActivity.this)
+                                .setOnImageSelectedListener(new TedBottomPicker.OnImageSelectedListener() {
+                                    @Override
+                                    public void onImageSelected(Uri uri) {
+                                        // here is selected uri
+                                    }
+                                })
+                                .create();
+
+                        tedBottomPicker.show(getSupportFragmentManager());
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                        Toast.makeText(ComidasActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+                    }
+
+
+                };
+                TedPermission.with(ComidasActivity.this)
+                        .setPermissionListener(permissionlistener)
+                        .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                        .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        .check();
+            }
+        });
+
+
 
 
     }
--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20182/lab1/MainActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20182/lab1/MainActivity.java
@@ -1,18 +1,10 @@
 package co.edu.udea.compumovil.gr04_20182.lab1;
 
-import android.Manifest;
-import android.net.Uri;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.gun0912.tedpermission.PermissionListener;
-import com.gun0912.tedpermission.TedPermission;
-
-
-import java.util.ArrayList;
-
-import gun0912.tedbottompicker.TedBottomPicker;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,35 +13,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PermissionListener permissionlistener = new PermissionListener() {
+        Button button = findViewById(R.id.button_comidas);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onPermissionGranted() {
-                Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
-                TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(MainActivity.this)
-                        .setOnImageSelectedListener(new TedBottomPicker.OnImageSelectedListener() {
-                            @Override
-                            public void onImageSelected(Uri uri) {
-                                // here is selected uri
-                            }
-                        })
-                        .create();
-
-                tedBottomPicker.show(getSupportFragmentManager());
+            public void onClick(View view) {
+                Intent activityIntent = new Intent(MainActivity.this, ComidasActivity.class);
+                MainActivity.this.startActivity(activityIntent);
             }
-
-            @Override
-            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
-                Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
-            }
-
-
-        };
-
-        TedPermission.with(this)
-                .setPermissionListener(permissionlistener)
-                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
-                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                .check();
+        });
 
 
     }
