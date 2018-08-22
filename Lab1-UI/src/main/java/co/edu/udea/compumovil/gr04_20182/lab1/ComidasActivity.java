06fe594edbc7cb5b5b04f0af9ef8544a28ef5700
Miguel Angel <manaranjoc@alcazares.edu.co>
2018-08-22 13:12:34 -0500

    Agregar libreria para manejo de galeria, actualizar minima version a 16 para compativilidad con esta. c

---
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20182/lab1/ComidasActivity.java
@@ -0,0 +1,17 @@
+package co.edu.udea.compumovil.gr04_20182.lab1;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ComidasActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_comidas);
+
+
+    }
+}
