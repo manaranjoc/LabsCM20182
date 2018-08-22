8ef84c58531f39fcdd5d8be127d78650995c8e6e
Miguel Angel <manaranjoc@alcazares.edu.co>
2018-08-22 17:58:45 -0500

    Actualizar imagen preview con imagen tomada de galeria

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20182/lab1/ComidasActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20182/lab1/ComidasActivity.java
@@ -3,11 +3,10 @@ package co.edu.udea.compumovil.gr04_20182.lab1;
 import android.Manifest;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.gun0912.tedpermission.PermissionListener;
@@ -18,25 +17,27 @@ import java.util.ArrayList;
 import gun0912.tedbottompicker.TedBottomPicker;
 
 public class ComidasActivity extends AppCompatActivity {
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comidas);
 
         Button button = findViewById(R.id.button_gallery);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 PermissionListener permissionlistener = new PermissionListener() {
                     @Override
                     public void onPermissionGranted() {
-                        Toast.makeText(ComidasActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
                         TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(ComidasActivity.this)
                                 .setOnImageSelectedListener(new TedBottomPicker.OnImageSelectedListener() {
                                     @Override
                                     public void onImageSelected(Uri uri) {
                                         // here is selected uri
+                                        ImageView imagePreview = findViewById(R.id.imagePreview);
+                                        imagePreview.setImageURI(uri);
                                     }
                                 })
                                 .create();
