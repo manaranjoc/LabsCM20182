2773015958723e02de303554e4e388a4201ffa34
Miguel Angel <manaranjoc@alcazares.edu.co>
2018-08-22 13:37:06 -0500

    Agregar libreria de manejo de permisos y hacer prueba de selección de fotos con MainActivity

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20182/lab1/MainActivity.java
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20182/lab1/MainActivity.java
@@ -1,7 +1,18 @@
 package co.edu.udea.compumovil.gr04_20182.lab1;
 
+import android.Manifest;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+
+import java.util.ArrayList;
+
+import gun0912.tedbottompicker.TedBottomPicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                TedBottomPicker tedBottomPicker = new TedBottomPicker.Builder(MainActivity.this)
+                        .setOnImageSelectedListener(new TedBottomPicker.OnImageSelectedListener() {
+                            @Override
+                            public void onImageSelected(Uri uri) {
+                                // here is selected uri
+                            }
+                        })
+                        .create();
+
+                tedBottomPicker.show(getSupportFragmentManager());
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                Toast.makeText(MainActivity.this, "Permission Denied\n" + deniedPermissions.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("If you reject permission,you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]")
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .check();
+
+
     }
 }
